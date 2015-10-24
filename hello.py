56281ced35cca0460be8d60e32e47620b4261022
Daniel Prado <pradosan@mit.edu>
2015-10-24 19:30:06 -0400

    init

--- hello.py
+++ hello.py
@@ -9,11 +9,11 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     """Respond to incoming requests."""
-    #myTest = praw.Reddit('myTest1')
-	#theContent = myTest.get_front_page(limit = 2)
-	#theList = [str(x) for x in theContent]
+    myTest = praw.Reddit('myTest1')
+	theContent = myTest.get_front_page(limit = 2)
+	theList = [str(x) for x in theContent]
     resp = twilio.twiml.Response()
     resp.say("Hello Monkey")
-    #resp.say(theList[0])
+    resp.say(theList[0])
  
     return str(resp)

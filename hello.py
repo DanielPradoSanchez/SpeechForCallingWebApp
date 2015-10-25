bfffa617c9fdb750b9ddda98f46cff76c7d64a46
Daniel Prado <pradosan@mit.edu>
2015-10-25 00:01:29 -0400

    init

--- hello.py
+++ hello.py
@@ -15,6 +15,5 @@ app = Flask(__name__)
 def hello():
     """Respond to incoming requests."""
     resp = twilio.twiml.Response()
-    resp.say('My name is Daniel Prado')
-    #resp.say('The top content on Reddit today is: ' + topContentTitle)
+    resp.say('The Monkey')
     return str(resp)

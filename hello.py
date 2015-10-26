ed2bb3386a12d8073151e479e94c1caa81452d9f
Daniel Prado <pradosan@mit.edu>
2015-10-26 00:37:00 -0400

    Test

--- hello.py
+++ hello.py
@@ -9,13 +9,12 @@ myName = 'Daniel Prado'
 reddit = praw.Reddit('danielprado')
 theContent = reddit.get_front_page(limit = 1)
 listOfContent = [str(x.title) for x in theContent]
-topContentTile = listOfContent[0]
+topContentTitle = listOfContent[0]
 
 @app.route("/", methods=['GET', 'POST'])
 def hello():
     """Respond to incoming requests."""
     resp = twilio.twiml.Response()
-    #resp.say('My name is ' + myName)
-    #resp.say('The top content on Reddit right now is ' + topContentTitle)
-    resp.say('hello monkey')
+    resp.say('My name is ' + myName)
+    resp.say('The top content on Reddit right now is ' + topContentTitle)
     return str(resp)

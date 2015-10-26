095cd34f93bb53684cda30e94f6f710cc9dcf0b7
Daniel Prado <pradosan@mit.edu>
2015-10-26 00:01:47 -0400

    Test

--- hello.py
+++ hello.py
@@ -3,18 +3,13 @@ from flask import Flask
 import twilio.twiml
 import praw
 
-myName = 'Daniel Prado'
-myTest = praw.Reddit('myTest1')
-topContent = myTest.get_front_page(limit = 1)
-listOfTopContentTitles = [x.title for x in topContent]
-topContentTitle = listOfTopContentTitles[0]
-
 app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def hello():
     """Respond to incoming requests."""
     resp = twilio.twiml.Response()
-    resp.say('My name is ' + myName)
-    resp.say('The top content on Reddit right now is ' + topContentTitle)
+    #resp.say('My name is ' + myName)
+    #resp.say('The top content on Reddit right now is ' + topContentTitle)
+    resp.say('What is up duuuuuuuuuude')
     return str(resp)

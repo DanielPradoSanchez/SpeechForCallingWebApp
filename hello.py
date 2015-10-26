db21c345e294246a1d57eddd0671cb476f4f5d18
Daniel Prado <pradosan@mit.edu>
2015-10-26 00:25:42 -0400

    Test

--- hello.py
+++ hello.py
@@ -9,7 +9,6 @@ app = Flask(__name__)
 def hello():
     """Respond to incoming requests."""
     resp = twilio.twiml.Response()
-    #resp.say('My name is ' + myName)
-    #resp.say('The top content on Reddit right now is ' + topContentTitle)
-    resp.say('What is up Garret')
+    resp.say('My name is ' + myName)
+    resp.say('The top content on Reddit right now is ' + topContentTitle)
     return str(resp)

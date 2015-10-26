34e508315c46363fa5e28da2788f1b61e7c53ddf
Daniel Prado <pradosan@mit.edu>
2015-10-26 11:17:54 -0400

    Commented

--- hello.py
+++ hello.py
@@ -1,3 +1,11 @@
+#
+#   A web app written with Flask and Praw. This web app
+#   displays the title of the top content on the front page
+#   of Reddit and a name. This web app is used for the calling 
+#   web app TwilCall which requires an xml format for the speech
+#   of the call
+#
+
 import os
 from flask import Flask
 import twilio.twiml
@@ -5,6 +13,8 @@ import praw
 
 app = Flask(__name__)
 
+# Setting up the name to be said and extracting the title
+# of the top content on Reddit
 myName = 'Daniel Prado'
 reddit = praw.Reddit('danielprado')
 theContent = reddit.get_front_page(limit = 1)
@@ -13,7 +23,7 @@ topContentTitle = listOfContent[0]
 
 @app.route("/", methods=['GET', 'POST'])
 def hello():
-    """Respond to incoming requests."""
+    # Respond to incoming requests
     resp = twilio.twiml.Response()
     resp.say('My name is ' + myName)
     resp.say('The top content on Reddit right now is ' + topContentTitle)

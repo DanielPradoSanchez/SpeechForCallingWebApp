30fc1e30cc8afc2fe9c404d73a7cc0a0af862b5b
Daniel Prado <pradosan@mit.edu>
2015-10-26 18:29:37 -0400

    got rid of title

--- hello.py
+++ hello.py
@@ -19,7 +19,7 @@ myName = 'Daniel Prado'
 reddit = praw.Reddit('danielprado')
 theContent = reddit.get_front_page(limit = 1)
 listOfContent = [str(x.title) for x in theContent]
-topContentTile = 'The title'
+topContentTitle = 'The title'
 #topContentTitle = listOfContent[0]
 
 @app.route("/", methods=['GET', 'POST'])

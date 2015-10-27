a6fa78f99780f46bb5cddefbbe9b3f1449569513
Daniel Prado <pradosan@mit.edu>
2015-10-26 21:59:15 -0400

    Deal with special characters

--- hello.py
+++ hello.py
@@ -18,9 +18,9 @@ app = Flask(__name__)
 # of the top content on Reddit
 myName = 'Daniel Prado'
 reddit = praw.Reddit('danielprado')
-theContent = reddit.get_front_page(limit = 1)
+theContent = reddit.get_front_page(limit = 3)
 listOfContent = [unidecode(x.title) for x in theContent]
-topContentTitle = listOfContent[0]
+topContentTitle = listOfContent[2]
 
 @app.route("/", methods=['GET', 'POST'])
 def hello():
